b6e307b78bedd74a4a46e00034673e5e5507858b
Aradhya <aradhyakurve36@gmail.com>
2026-07-28 02:57:38 +0530

    Add files via upload

---
+++ Encryption program.py
@@ -0,0 +1,35 @@
+# Encryption program
+
+import random
+
+import string
+
+chars = string.punctuation + string.digits + string.ascii_letters + " "
+chars = list(chars)
+key = chars.copy()
+
+random.shuffle(key)
+
+#print(chars)
+
+#ENCRYPTION
+
+plain_text = input("Enter a message to encrypt : ")
+cipher_text = ""
+
+for letter in plain_text :
+    index = chars.index(letter)
+    cipher_text += key[index]
+
+print(f"Encrypted message : {cipher_text}")
+
+#DECRYPTION
+
+cipher_text = input("Enter a message to encrypt : ")
+plain_text = ""
+
+for letter in cipher_text :
+    index = key.index(letter)
+    plain_text += chars[index]
+
+print(f"Decrypted message : {plain_text}")
